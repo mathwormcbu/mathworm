21864e1881606e181f75381f26fc4a8b1d48b8f9
Seher Aydın <45860237+seheraydin@users.noreply.github.com>
2020-01-02 19:31:59 +0300

    Add files via upload

--- views/maintance/footer.php
+++ views/maintance/footer.php
@@ -7,4 +7,6 @@
 	<script type="text/javascript" src="<?= __URLS__ ?>assets/js/bootstrap.js"></script>
 	<script type="text/javascript" src="<?= __URLS__ ?>assets/js/toastr.min.js"></script>
 	<script type="text/javascript" src="<?= __URLS__ ?>assets/js/SlimWhizzy.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.js"></script>
+	
 </html>
--- views/maintance/header.php
+++ views/maintance/header.php
@@ -34,6 +34,7 @@
 	<link rel="stylesheet" type="text/css" href="<?= __URLS__ ?>assets/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
 	<link rel="stylesheet" type="text/css" href="<?= __URLS__ ?>assets/css/toastr.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.css">
 </head>
 
 <body>
